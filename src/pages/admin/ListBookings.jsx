0a31c2418e2fca4e78b6e5326b522bf9be2f44c9
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-07-19 10:50:37 +0600

    connect backend api with listbooking page

--- src/pages/admin/ListBookings.jsx
+++ src/pages/admin/ListBookings.jsx
@@ -1,54 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import { dummyBookingData } from '../../assets/assets';
-import Loading from '../../components/Loading';
-import Title from '../../components/admin/Title';
-import { dateFormat } from '../../lib/dateFormat';
+import React, { useEffect, useState } from "react";
+import { dummyBookingData } from "../../assets/assets";
+import Loading from "../../components/Loading";
+import Title from "../../components/admin/Title";
+import { dateFormat } from "../../lib/dateFormat";
+import { useAppContext } from "../../context/AppContext";
 
 const ListBookings = () => {
-  const currency = import.meta.env.VITE_CURRENCY
-  const [bookings, setBookings]= useState([]);
-  const[loading, setLoading] = useState(true)
-  const getAllBookings = ()=>{
-    setBookings(dummyBookingData)
-    setLoading(false)
-  }
+  const currency = import.meta.env.VITE_CURRENCY;
+  const { axios, getToken, user } = useAppContext();
 
-  useEffect(()=>{
-    getAllBookings()
-  }, [])
+  const [bookings, setBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getAllBookings = async () => {
+    try {
+      const { data } = await axios.get("/admin/all-bookings", {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+      console.log(data);
+      setBookings(data.bookings);
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  };
 
-  return !loading? (
+  useEffect(() => {
+    if (user) {
+      getAllBookings();
+    }
+  }, []);
+
+  return !loading ? (
     <>
-    <Title text1='List' text2='Bookings' />
-    <div className='max-w-4xl mt-6 overflow-x-auto'>
-      <table className='w-full border-collapse rounded-md overflow-hidden text-nowrap'>
-        <thead>
-          <tr className='bg-primary/20  text-left text-white'>
-          <th className='p-2 font-medium pl-5'>User Name</th>
-          <th className='p-2 font-medium '>Movie Name</th>
-            <th className='p-2 font-medium'>Show Time</th>
-            <th className='p-2 font-medium'>Seats</th>
-            <th className='p-2 font-medium'>Amount</th>
-          </tr>
-        </thead>
-        <tbody className='text-sm font-light'>
-        {
-          bookings.map((item, idx)=>(
-            <tr key={idx} className='border-b border-primary/10'>
-              <td className='p-2 min-w-45 pl-5'> {item.user.name} </td>
-              <td className='p-2 min-w-45'> {item.show.movie.title} </td>
-              <td className='p-2'> {dateFormat(item.show.showDateTime)} </td>
-              <td className='p-2'> {Object.keys(item?.bookedSeats || {}).map(seat => item.bookedSeats[seat]).join(", ")}
- </td>
-              <td className='p-2'> {currency} {item.amount} </td>
+      <Title text1="List" text2="Bookings" />
+      <div className="max-w-4xl mt-6 overflow-x-auto">
+        <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
+          <thead>
+            <tr className="bg-primary/20  text-left text-white">
+              <th className="p-2 font-medium pl-5">User Name</th>
+              <th className="p-2 font-medium ">Movie Name</th>
+              <th className="p-2 font-medium">Show Time</th>
+              <th className="p-2 font-medium">Seats</th>
+              <th className="p-2 font-medium">Amount</th>
             </tr>
-          ))
-        }
-      </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody className="text-sm font-light">
+            {bookings.map((item, idx) => (
+              <tr key={idx} className="border-b border-primary/10">
+                <td className="p-2 min-w-45 pl-5"> {item.user.name} </td>
+                <td className="p-2 min-w-45"> {item.show.movie.title} </td>
+                <td className="p-2"> {dateFormat(item.show.showDateTime)} </td>
+                <td className="p-2">
+                  {" "}
+                  {Object.keys(item?.bookedSeats || {})
+                    .map((seat) => item.bookedSeats[seat])
+                    .join(", ")}
+                </td>
+                <td className="p-2">
+                  {" "}
+                  {currency} {item.amount}{" "}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  ) : <Loading/>
-}
+  ) : (
+    <Loading />
+  );
+};
 
-export default ListBookings
+export default ListBookings;
--- src/pages/admin/ListShow.jsx
+++ src/pages/admin/ListShow.jsx
@@ -3,24 +3,24 @@ import { dummyShowsData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import { dateFormat } from '../../lib/dateFormat';
+import { useAppContext } from '../../context/AppContext';
 
 const ListShow = () => {
-
+  const {axios, getToken, user} = useAppContext();
   const currency = import.meta.env.VITE_CURRENCY
   const [shows, setShows]= useState([]);
   const[loading, setLoading] = useState(true)
   const getAllShows = async() =>{
     try {
-      setShows([{
-        movie:dummyShowsData[0],
-        showDateTime:"2025-06-30T02:30:00.000Z",
-        showPrice: 59,
-        occupiedSeats:{
-          A1: "user_1",
-          B1: "user_2",
-          C1: "user_3"
+      const {data} = await axios.get('/admin/all-shows',{
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
         }
-      }]);
+      })
+      console.log(data)
+      setShows(data.shows);
+      console.log(data.shows[0])
+    console.log(shows);
       setLoading(false)
     } catch (error) {
       console.error(error);
@@ -29,8 +29,10 @@ const ListShow = () => {
   }
 
   useEffect(()=>{
-    getAllShows()
-  },[])
+    if(user){
+      getAllShows()
+    }
+  },[user])
 
   return !loading ? (
     <>
